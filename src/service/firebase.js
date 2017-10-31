17d740de527b7c7763f7505f2102d2f5923d507e
Jereme Allen <jereme.allen@wheniwork.com>
2017-10-31 18:39:52 -0500

    Add in firebase credentials

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,7 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-
+  apiKey: 'AIzaSyBuPZzoMLCB8yQBKRA8qa7vt3okPjm8-NY',
+  authDomain: 'cropchat-18f8a.firebaseapp.com',
+  databaseURL: 'https://cropchat-18f8a.firebaseio.com',
+  storageBucket: 'cropchat-18f8a.appspot.com',
+  projectId: 'cropchat-18f8a',
+  messagingSenderId: '136274533795',
 };
 
 firebase.initializeApp(config);
